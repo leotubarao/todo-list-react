08802bd7f9264efab9e64822ca42afd0347fd8a8
leotubarao <l.tubarao.santos@gmail.com>
2019-04-07 10:43:32 -0300

    Updates

---
+++ precache-manifest.c9364b28bdb81c2633aee419b14ef283.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35e3544cd546257cfadf",
+    "url": "/todo-list-react/static/js/runtime~main.bb4d8c92.js"
+  },
+  {
+    "revision": "633f17aefb0845584ce0",
+    "url": "/todo-list-react/static/js/main.c7537359.chunk.js"
+  },
+  {
+    "revision": "4b755debe388e4746b31",
+    "url": "/todo-list-react/static/js/2.37c6651f.chunk.js"
+  },
+  {
+    "revision": "4b755debe388e4746b31",
+    "url": "/todo-list-react/static/css/2.e123abec.chunk.css"
+  },
+  {
+    "revision": "57c6f37963082c728f9bdfbdbbd0d85b",
+    "url": "/todo-list-react/index.html"
+  }
+];
